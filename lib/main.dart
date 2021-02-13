1205e487073ffc750a989943945d61aa9e7ee5e5
Ankit <1ankitmaurya1@gmail.com>
2021-02-13 21:08:08 +0530

    modified question and main file add 3 new file answer.dart,quiz.dart,result.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,25 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s is your fevorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s is your fevorite Animal?',
+      'answers': ['Horese', 'Cat', 'Dog', 'Loin']
+    },
+    {
+      'questionText': 'What\'s is your fevorite bird?',
+      'answers': ['Peekock', 'Parrot', 'Sparrow', 'Kingfisher']
+    },
+    {
+      'questionText': 'What\'s is your fevorite fruit?',
+      'answers': ['Mango', 'Apple', 'Banana', 'Grapes']
+    },
+  ];
 
   var _questionIndex = 0;
 
@@ -22,43 +41,35 @@ class _MyAppState extends State<MyApp>{
     setState(() {
       _questionIndex++;
     });
-    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('we have more question');
+    } else {
+      print('question khatm');
+    }
   }
 
   @override
   Widget build(BuildContext contaxt) {
-    var questions = [
-      'What\'s is your fevorite color?',
-      'What\'s is your fevorite animal?',
-    ];
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.green,
-              title: Text('My Quiz App'),
-            ),
-            body: Column(
-              children: [
-                Text(questions[_questionIndex]),
-                RaisedButton(
-                  elevation: 10.0,
-                  color: Colors.blueAccent,
-                  textColor: Color.alphaBlend(Colors.white, Colors.blue),
-                  child: Text('Answer 1'),
-                  onPressed: _answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Answer 2'),
-                  onPressed: _answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Answer 3'),
-                  onPressed: _answerQuestion,
-                ),
-              ],
-            ),
-          ),
-        );
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('My Quiz App'),
+        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it'),
+              ),
+      ),
+    );
   }
 }
-
--- lib/question.dart
+++ lib/question.dart
@@ -6,6 +6,14 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20.0),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 24),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
