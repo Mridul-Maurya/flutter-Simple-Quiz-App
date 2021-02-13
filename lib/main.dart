6d044323541b9687acbe18f455e9be81da664bf4
Ankit <1ankitmaurya1@gmail.com>
2021-02-13 14:18:45 +0530

    adding button in main

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext contaxt) {
+    var questions = [
+      'What\'s is your fevorite color?',
+      'What\'s is your fevorite animal?',
+    ];
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Color.fromRGBO(100, 150, 200, 70.0),
-      appBar: AppBar(
-        title: Text('My Quiz App'),
-      ),body: Text('this is my default text',
-      style:TextStyle(color: Colors.white),),
-    ));
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              title: Text('My Quiz App'),
+            ),
+            body: Column(
+              children: [
+                Text('The question!'),
+                RaisedButton(
+                  child: Text('Answer 1'),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text('Answer 3'),
+                  onPressed: null,
+                ),
+              ],
+            )));
   }
 }
